c234ddfc61c74afbb028a2cc91e1541396173ea1
aashnamehta <30861890+aashnamehta@users.noreply.github.com>
2020-12-07 15:17:05 +0530

    logic changes and restructuring

--- config-script/lamp_cm.sh
+++ config-script/lamp_cm.sh
@@ -5,13 +5,13 @@ sudo apt-get update
 
 if [ -s deb_packages/uninstall.txt ]; then
   
-  declare -a removal_list
-  while IFS='\n' read -r value; do
-    removal_list+=( "${value}" )
+  declare -a rm_list
+  while IFS='\n' read -r rm_value; do
+    rm_list+=( "${rm_value}" )
   done < "deb_packages/uninstall.txt"
 
  
-  for rm_pkg in "${removal_list[@]}"
+  for rm_pkg in "${rm_list[@]}"
   do
     if dpkg -l | grep -i "${rm_pkg}"; then
       sudo apt-get remove "${rm_pkg}" 
@@ -26,8 +26,8 @@ fi
 if [ -s deb_packages/install.txt ]; then
   
   declare -a install_list
-  while IFS='\n' read -r value; do
-    install_list+=( "${value}" )
+  while IFS='\n' read -r install_value; do
+    install_list+=( "${install_value}" )
   done < "deb_packages/install.txt"
 
  
@@ -53,16 +53,22 @@ if [ -f "/var/www/html/index.html" ]; then
 fi
 
 
-if [ -s metadata.txt ]; then
+if [ -s metadata-userdata/metadata.txt ]; then
   declare -A metadata
   while IFS== read -r key value; do
     metadata[$key]=$value
-  done < "metadata.txt"
+  done < "metadata-userdata/metadata.txt"
   sudo chmod "${metadata[permissions]}" "${metadata[file]}"
   sudo chown "${metadata[owner]}" "${metadata[file]}"
   sudo chgrp "${metadata[group]}" "${metadata[file]}"
-  sudo echo "${metadata[content]}" > "${metadata[file]}"
 fi
 
+if [ -s metadata-userdata/php-file-content.txt ]; then
+  declare -A userdata
+  while IFS== read -r key value; do
+   userdata[$key]=$value
+  done < "metadata-userdata/php-file-content.txt"
+  sudo echo "${userdata[content]}" > "${userdata[file]}"
+fi
 
 needrestart
