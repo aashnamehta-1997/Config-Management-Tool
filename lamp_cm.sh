cae05524bb3874cbf236bebc449e43cfee868e3a
aashnamehta <30861890+aashnamehta@users.noreply.github.com>
2020-12-10 07:38:44 +0530

    Update lamp_cm.sh

--- lamp_cm.sh
+++ lamp_cm.sh
@@ -16,25 +16,25 @@ if [ -s deb-packages/uninstall.txt ]; then
 # The value (package name to be uninstalled) is added to the removal list
     rm_list+=( "${value}" )
     
-# Fetching the contents from file uninstall.txt
+# Fetches the contents from file uninstall.txt
   done < "deb-packages/uninstall.txt"
 
 
-# To remove the packages from the removal list 
+# Removes the packages from the removal list 
   for rm_pkg in "${rm_list[@]}"
   do
     if dpkg -l | grep -i "${rm_pkg}"; then
     
-      # To remove a particular package 
+      # Removes a particular package 
       sudo apt-get remove "${rm_pkg}" 
       
       # Removes all the files or packages that were downloaded as a dependency to this package
       sudo apt-get autoremove 
       
-      # To remove any residual package
+      # Removes any residual package
       sudo apt-get purge -y $(dpkg --list |grep '^rc' |awk '{print $2}') 
       
-      # To remove the retrieved packages from the local cache
+      # Removes the retrieved packages from the local cache
       sudo apt-get clean 
     fi
   done
@@ -49,22 +49,22 @@ if [ -s deb-packages/install.txt ]; then
   # Internal File Separator seperates the packages linewise and stores them in var value 
   while IFS='\n' read -r value; do
       
-    # The value (package name to be installed) is added to the removal list  
+    # The value (package name to be installed) is added to the install list  
     install_list+=( "${value}" )
   
-  # Fetching the contents from uninstall.txt
+  # Fetches the contents from uninstall.txt
   done < "deb-packages/install.txt"
 
   # Installation of packages from the install.txt
   for in_pkg in "${install_list[@]}"
   do
-    # Checking if the packages to be installed are present in the package manager
+    # Checks if the packages to be installed are present in the package manager
     if ! dpkg -l | grep "${in_pkg}"; then
     
-      # Installing the package mentioned in the install_list
+      # Installs the package mentioned in the install_list
       sudo apt-get install -y "${in_pkg}"
       
-      # Installing mysql-server dependencies
+      # Installs mysql-server dependencies
       if [ "${in_pkg}" == "mysql-server" ]; then
        
         sudo mysql_install_db
@@ -75,46 +75,46 @@ if [ -s deb-packages/install.txt ]; then
   done
 fi
 
-# Check if the file index.html exists 
+# Checks if the file index.html exists or not
 if [ -f "/var/www/html/index.html" ]; then
-  # Removing index.html
+  # Removes index.html
   sudo rm /var/www/html/index.html
-  # Creating index.php on the same location as that of index.html
+  # Creates index.php on the same location as that of index.html
   touch /var/www/html/index.php
 fi
 
-# To check if the metadata file containing the package permissions exists or not
+# Checks if the metadata file containing the package permissions exists or not
 if [ -s metadata-userdata/metadata.txt ]; then
 
-# To declare an Associative Array for fetching the metadata
+# Declares an Associative Array for fetching the metadata
   declare -A metadata
   
-  # Internal File Separator used to get key and value and store it in an associative array
+  # Internal File Separator is used to get key and value and stores it in an associative array
   while IFS== read -r key value; do
     metadata[$key]=$value
   done < "metadata-userdata/metadata.txt"
-  # To modify the permission and set it to 644 
+  # Modifies the permission and set it to 644 
   sudo chmod "${metadata[permissions]}" "${metadata[file]}"
-  # To modify the owner name 
+  # Modifies the owner name 
   sudo chown "${metadata[owner]}" "${metadata[file]}"
-  # To modify the group name
+  # Modifies the group name
   sudo chgrp "${metadata[group]}" "${metadata[file]}"
 fi
 
-# To declare an Associative Array for fetching the userdata
+# Declares an Associative Array for fetching the userdata
 if [ -s metadata-userdata/userdata.txt ]; then 
 
-  # To declare an Associative Array for fetching the userdata
+  # Declares an Associative Array for fetching the userdata
   declare -A userdata
   
-  # Internal File Separator used to get key and value and store it in an associative array
+  # Internal File Separator is used to get key and value and store it in an associative array
   while IFS== read -r key value; do
     userdata[$key]=$value
     
-  # Fetching the contents from file uninstall.txt
+  # Fetches the contents from file uninstall.txt
   done < "metadata-userdata/userdata.txt"
   
-  # Storing the php file content in index.php
+  # Stores the php file content in index.php
   sudo echo "${userdata[content]}" > "${userdata[file]}"
 fi
 
